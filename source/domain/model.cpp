e190854b77edca8dc9fc067c98445a37b5aacb32
Sergey Kuznetsov <kuzzz99@gmail.com>
2019-03-23 23:22:39 +0300

    Fix tests. Fix synchronization error

--- source/domain/model.cpp
+++ source/domain/model.cpp
@@ -1,170 +1,159 @@
 #include "domain/model.h"
 
-#include <unordered_map>
-#include <thread>
 #include <mutex>
+#include <thread>
+#include <unordered_map>
 
 #include "domain/ball.h"
 #include "domain/physics.h"
 
 namespace domain {
 
-struct Model::Impl
-{
-    Impl();
-    ~Impl();
+struct Model::Impl {
+  Impl();
+  ~Impl();
 
-    void simulate();
-    void stopSimulation();
+  void simulate();
+  void stopSimulation();
 
-    std::unordered_map<std::size_t, BallPtr> _idToBallMap;
-    mutable std::mutex _mutex;
-    bool _simulationIsActive = false;
-    std::unique_ptr<std::thread> _thread;
-    float _deltaT = 0.001f;
+  std::unordered_map<std::size_t, BallPtr> _idToBallMap;
+  mutable std::mutex _idToBallMapMutex;
+  mutable std::mutex _mutex;
+  bool _simulationIsActive = false;
+  std::unique_ptr<std::thread> _thread;
+  float _deltaT = 0.001f;
 };
 
-Model::Model():
-    _d(std::make_unique<Impl>())
-{}
+Model::Model() : _d(std::make_unique<Impl>()) {}
 
 Model::~Model() = default;
 
-utils::Id Model::addBall(const utils::Vector2f& position)
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    const auto newBall = std::make_shared<Ball>(position);
-    _d->_idToBallMap.emplace(newBall->id().toStdSizeT(), newBall);
-    return newBall->id();
+utils::Id Model::addBall(const utils::Vector2f& position) {
+  std::lock_guard<std::mutex> lock(_d->_idToBallMapMutex);
+  const auto newBall = std::make_shared<Ball>(position);
+  _d->_idToBallMap.emplace(newBall->id().toStdSizeT(), newBall);
+  return newBall->id();
 }
 
-void Model::removeBall(const utils::Id& ballId)
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    _d->_idToBallMap.erase(_d->_idToBallMap.find(ballId.toStdSizeT()));
+void Model::removeBall(const utils::Id& ballId) {
+  std::lock_guard<std::mutex> lock(_d->_idToBallMapMutex);
+  _d->_idToBallMap.erase(_d->_idToBallMap.find(ballId.toStdSizeT()));
 }
 
-void Model::removeBall(const utils::Vector2f &position)
-{
-    const auto ballId = findBallByPosition(position);
-    if (!ballId.isNull()) removeBall(ballId);
+void Model::removeBall(const utils::Vector2f& position) {
+  const auto ballId = findBallByPosition(position);
+  if (!ballId.isNull()) removeBall(ballId);
 }
 
-void Model::moveBall(const utils::Id& ballId, const utils::Vector2f& position)
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    _d->_idToBallMap.at(ballId.toStdSizeT())->setPosition(position);
+void Model::moveBall(const utils::Id& ballId, const utils::Vector2f& position) {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  _d->_idToBallMap.at(ballId.toStdSizeT())->setPosition(position);
 }
 
-void Model::setBallFixed(const utils::Id& ballId, const bool fixed)
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    if (!fixed) _d->_idToBallMap.at(ballId.toStdSizeT())->setSpeed(utils::Vector2f());
-    _d->_idToBallMap.at(ballId.toStdSizeT())->setFixed(fixed);
+void Model::setBallFixed(const utils::Id& ballId, const bool fixed) {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  if (!fixed)
+    _d->_idToBallMap.at(ballId.toStdSizeT())->setSpeed(utils::Vector2f());
+  _d->_idToBallMap.at(ballId.toStdSizeT())->setFixed(fixed);
 }
 
-utils::Id Model::findBallByPosition(const utils::Vector2f& position) const
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    for (const auto& idAndBall : _d->_idToBallMap) {
-        if ((idAndBall.second->position() - position).normSquare() < Ball::RADIUS_SQUARE)
-            return idAndBall.first;
+utils::Id Model::findBallByPosition(const utils::Vector2f& position) const {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  for (const auto& idAndBall : _d->_idToBallMap) {
+    if ((idAndBall.second->position() - position).normSquare() <
+        Ball::RADIUS_SQUARE)
+      return idAndBall.first;
+  }
+  return utils::Id::NULLID;
+}
+
+std::vector<utils::Vector2f> Model::ballsPositions() const {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  std::vector<utils::Vector2f> result;
+  for (const auto& idAndBall : _d->_idToBallMap) {
+    result.emplace_back(idAndBall.second->position());
+  }
+  return result;
+}
+
+void Model::startStopSimulation() {
+  if (!_d->_simulationIsActive) {
+    _d->_simulationIsActive = true;
+    _d->_thread =
+        std::make_unique<std::thread>(&Impl::simulate, std::ref(*_d.get()));
+  } else {
+    _d->_simulationIsActive = false;
+    for (auto& idAndBall : _d->_idToBallMap)
+      idAndBall.second->setSpeed(utils::Vector2f());
+    _d->stopSimulation();
+  }
+}
+
+float Model::deltaT() const { return _d->_deltaT; }
+
+void Model::setDeltaT(float deltaT) {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  _d->_deltaT = deltaT;
+}
+
+std::size_t Model::ballsNumber() const {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  return _d->_idToBallMap.size();
+}
+
+std::vector<utils::Id> Model::ballIds() const {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  std::vector<utils::Id> result;
+  for (const auto& idAndBall : _d->_idToBallMap) {
+    result.push_back(idAndBall.first);
+  }
+  return result;
+}
+
+utils::Vector2f Model::ballPosition(const utils::Id& ballId) const {
+  std::lock_guard<std::mutex> lock(_d->_mutex);
+  return _d->_idToBallMap.at(ballId.toStdSizeT())->position();
+}
+
+Model::Impl::Impl() {
+  const std::size_t ballsInitialNumber = 2;
+  for (std::size_t i = 0; i < ballsInitialNumber; i++) {
+    const auto newBall = std::make_shared<Ball>();
+    _idToBallMap.emplace(newBall->id().toStdSizeT(), newBall);
+  }
+}
+
+Model::Impl::~Impl() {
+  if (_simulationIsActive) stopSimulation();
+}
+
+void Model::Impl::simulate() {
+  while (_simulationIsActive) {
+    {
+      std::lock_guard<std::mutex> lock_map(_idToBallMapMutex);
+      for (auto& idAndBall : _idToBallMap) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        const auto force =
+            Physics::calculateForceForBall(idAndBall.second, _idToBallMap);
+        idAndBall.second->applyForce(force, Impl::_deltaT);
+      }
     }
-    return utils::Id::NULLID;
-}
 
-std::vector<utils::Vector2f> Model::ballsPositions() const
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    std::vector<utils::Vector2f> result;
-    for (const auto& idAndBall : _d->_idToBallMap) {
-        result.emplace_back(idAndBall.second->position());
+    {
+      std::lock_guard<std::mutex> lock_map(_idToBallMapMutex);
+      for (auto& idAndBall : _idToBallMap) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        idAndBall.second->makeStep(Impl::_deltaT);
+      }
     }
-    return result;
+  }
 }
 
-void Model::startStopSimulation()
-{
-    if (!_d->_simulationIsActive)  {
-        _d->_simulationIsActive = true;
-        _d->_thread = std::make_unique<std::thread>(&Impl::simulate, std::ref(*_d.get()));
-    } else {
-        _d->_simulationIsActive = false;
-        for (auto& idAndBall : _d->_idToBallMap)
-            idAndBall.second->setSpeed(utils::Vector2f());
-        _d->stopSimulation();
-    }
+void Model::Impl::stopSimulation() {
+  _mutex.lock();
+  _simulationIsActive = false;
+  _mutex.unlock();
+  _thread->join();
 }
 
-float Model::deltaT() const
-{
-  return _d->_deltaT;
-}
-
-void Model::setDeltaT(float deltaT)
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    _d->_deltaT = deltaT;
-}
-
-std::size_t Model::ballsNumber() const
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    return _d->_idToBallMap.size();
-}
-
-std::vector<utils::Id> Model::ballIds() const
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    std::vector<utils::Id> result;
-    for (const auto& idAndBall : _d->_idToBallMap) {
-        result.push_back(idAndBall.first);
-    }
-    return result;
-}
-
-utils::Vector2f Model::ballPosition(const utils::Id& ballId) const
-{
-    std::lock_guard<std::mutex> lock(_d->_mutex);
-    return _d->_idToBallMap.at(ballId.toStdSizeT())->position();
-}
-
-Model::Impl::Impl()
-{
-   const std::size_t ballsInitialNumber = 2;
-   for (std::size_t i = 0; i < ballsInitialNumber; i++) {
-       const auto newBall = std::make_shared<Ball>();
-       _idToBallMap.emplace(newBall->id().toStdSizeT(), newBall);
-   }
-}
-
-Model::Impl::~Impl()
-{
-    if (_simulationIsActive) stopSimulation();
-}
-
-void Model::Impl::simulate()
-{
-    while (_simulationIsActive) {
-        for (auto& idAndBall : _idToBallMap) {
-            std::lock_guard<std::mutex> lock(_mutex);
-            const auto force = Physics::calculateForceForBall(idAndBall.second, _idToBallMap);
-            idAndBall.second->applyForce(force, Impl::_deltaT);
-        }
-
-        for (auto& idAndBall : _idToBallMap) {
-            std::lock_guard<std::mutex> lock(_mutex);
-            idAndBall.second->makeStep(Impl::_deltaT);
-        }
-    }
-}
-
-void Model::Impl::stopSimulation()
-{
-    _mutex.lock();
-    _simulationIsActive = false;
-    _mutex.unlock();
-    _thread->join();
-}
-
-} // namespace domain
+}  // namespace domain
--- test/domain/model_test.cpp
+++ test/domain/model_test.cpp
@@ -3,95 +3,91 @@
 #include <gtest/gtest.h>
 #include <math.h>
 #include <stdlib.h>
+#include <thread>
 
 #include "test_extesions.h"
 #include "utils/id.h"
 
-#include "domain/ball.h"
 #include "common/field_sizes.h"
+#include "domain/ball.h"
 
 using namespace domain;
 
-class Model_Test: public ::testing::Test
-{
-protected:
-   Model model;
-   void SetUp()
-   {
-       model.setDeltaT(0.01f);
-   }
-   void clearModel() {
-        for (const auto& ballId : model.ballIds()) {
-            model.removeBall(ballId);
-        }
-   }
+class Model_Test : public ::testing::Test {
+ protected:
+  Model model;
+  void SetUp() { model.setDeltaT(0.01f); }
+  void clearModel() {
+    for (const auto& ballId : model.ballIds()) {
+      model.removeBall(ballId);
+    }
+  }
 };
 
-TEST_F(Model_Test, BallOperations)
-{
-    clearModel();
-    const auto ballPosition = utils::Vector2f();
-    model.addBall(ballPosition);
-    EXPECT_EQ(model.ballsNumber(), 1);
-    EXPECT_FALSE(model.findBallByPosition(ballPosition).isNull());
+TEST_F(Model_Test, BallOperations) {
+  clearModel();
+  const auto ballPosition = utils::Vector2f(25.f, 35.f);
+  model.addBall(ballPosition);
+  EXPECT_EQ(model.ballsNumber(), 1);
+  EXPECT_FALSE(model.findBallByPosition(ballPosition).isNull());
 
-    const auto newBallPosition = utils::Vector2f(100.f, 500.f);
-    model.moveBall(model.ballIds().at(0), newBallPosition);
-    model.removeBall(ballPosition);
-    EXPECT_EQ(model.ballsNumber(), 1);
-    model.removeBall(newBallPosition);
-    EXPECT_EQ(model.ballsNumber(), 0);
+  const auto newBallPosition = utils::Vector2f(100.f, 500.f);
+  model.moveBall(model.ballIds().at(0), newBallPosition);
+  model.removeBall(ballPosition);
+  EXPECT_EQ(model.ballsNumber(), 1);
+  model.removeBall(newBallPosition);
+  EXPECT_EQ(model.ballsNumber(), 0);
 }
 
-TEST_F(Model_Test, SimulationRunning)
-{
-    const auto initialBallsNumber = model.ballsNumber();
-    model.startStopSimulation();
+TEST_F(Model_Test, SimulationRunning) {
+  const auto initialBallsNumber = model.ballsNumber();
+  model.startStopSimulation();
 
-    EXPECT_NO_THROW(model.addBall(utils::Vector2f()));
-    EXPECT_EQ(model.ballsNumber(), initialBallsNumber + 1);
+  EXPECT_NO_THROW(model.addBall(utils::Vector2f()));
+  EXPECT_EQ(model.ballsNumber(), initialBallsNumber + 1);
 
-    EXPECT_NO_THROW(model.removeBall(utils::Vector2f()));
+  EXPECT_NO_THROW(model.removeBall(utils::Vector2f()));
 
-    const auto ballId = model.ballIds().at(0);
-    model.setBallFixed(ballId, true);
-    const auto newBallPosition = utils::Vector2f(100.f, 300.f);
-    model.moveBall(ballId, newBallPosition);
-    EXPECT_VECTORS_EQ(model.ballPosition(ballId), newBallPosition);
-    EXPECT_NO_THROW(model.removeBall(ballId));
+  const auto ballId = model.ballIds().at(0);
+  model.setBallFixed(ballId, true);
+  const auto newBallPosition = utils::Vector2f(100.f, 300.f);
+  model.moveBall(ballId, newBallPosition);
+  EXPECT_VECTORS_EQ(model.ballPosition(ballId), newBallPosition);
+  EXPECT_NO_THROW(model.removeBall(ballId));
 }
 
-TEST_F(Model_Test, BallsMoving)
-{
-    clearModel();
-    const std::vector<utils::Vector2f> ballsPositions = {utils::Vector2f(100.f, 100.f), utils::Vector2f(101.f, 100.f),
-                                                         utils::Vector2f(100.5f, 100.f + std::sqrt(0.75f))};
-    std::vector<utils::Id> ballIds;
-    for (const auto& position : ballsPositions) {
-        ballIds.emplace_back(model.addBall(position));
-    }
-    model.startStopSimulation();
-    sleep(1);
+TEST_F(Model_Test, BallsMoving) {
+  clearModel();
+  const std::vector<utils::Vector2f> ballsPositions = {
+      utils::Vector2f(100.f, 100.f), utils::Vector2f(101.f, 100.f),
+      utils::Vector2f(100.5f, 100.f + std::sqrt(0.75f))};
+  std::vector<utils::Id> ballIds;
+  for (const auto& position : ballsPositions) {
+    ballIds.emplace_back(model.addBall(position));
+  }
+  model.startStopSimulation();
+  sleep(1);
 
-    for (std::size_t i = 0; i < ballIds.size(); i++) {
-        EXPECT_VECTORS_EQ(model.ballPosition(ballIds[i]), ballsPositions[i]);
-    }
+  for (std::size_t i = 0; i < ballIds.size(); i++) {
+    EXPECT_VECTORS_EQ(model.ballPosition(ballIds[i]), ballsPositions[i]);
+  }
 
-    model.addBall(utils::Vector2f(101.f, 101.f));
-    sleep(1);
+  model.addBall(utils::Vector2f(101.f, 101.f));
+  sleep(1);
 
-    for (std::size_t i = 0; i < ballIds.size(); i++) {
-        EXPECT_VECTORS_NE(model.ballPosition(ballIds[i]), ballsPositions[i]);
-    }
+  for (std::size_t i = 0; i < ballIds.size(); i++) {
+    EXPECT_VECTORS_NE(model.ballPosition(ballIds[i]), ballsPositions[i]);
+  }
 }
 
-TEST_F(Model_Test, LongSimulation)
-{
-    model.setDeltaT(10.f);
-    model.startStopSimulation();
-    sleep(30);
-    for (const auto& ballPosition : model.ballsPositions()) {
-        EXPECT_TRUE(ballPosition.x() > Ball::RADIUS && ballPosition.x() < FIELD_WIDTH - Ball::RADIUS);
-        EXPECT_TRUE(ballPosition.y() > Ball::RADIUS && ballPosition.y() < FIELD_HEIGHT - Ball::RADIUS);
-    }
+TEST_F(Model_Test, LongSimulation) {
+  model.setDeltaT(10.f);
+  model.startStopSimulation();
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  for (const auto& ballPosition : model.ballsPositions()) {
+    EXPECT_TRUE(ballPosition.x() > Ball::RADIUS &&
+                ballPosition.x() < FIELD_WIDTH - Ball::RADIUS);
+    EXPECT_TRUE(ballPosition.y() > Ball::RADIUS &&
+                ballPosition.y() < FIELD_HEIGHT - Ball::RADIUS);
+  }
 }
